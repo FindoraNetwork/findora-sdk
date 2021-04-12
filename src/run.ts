0159e5a28a912dd87b3a9fb80a972ffc59607144
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2021-04-12 17:19:47 -0400

    Add a scaffolding sdk implementation

---
+++ src/run.ts
@@ -0,0 +1,5 @@
+import { Random } from './random';
+
+const data = Random.getBytes(12);
+
+console.log('our data is:', data);
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+const commonConfig = {
+  mode: 'production',
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  plugins: [
+    new webpack.IgnorePlugin({
+      resourceRegExp: /wordlists\/(french|spanish|italian|korean|chinese_simplified|chinese_traditional|japanese)\.json$/,
+    }),
+  ],
+};
+
+const webConfig = {
+  ...commonConfig,
+  target: 'web',
+  output: {
+    filename: 'bundle.js',
+    libraryTarget: 'umd',
+    library: 'Terra',
+  },
+  resolve: {
+    ...commonConfig.resolve,
+    fallback: {
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer'),
+    },
+  },
+  plugins: [...commonConfig.plugins],
+};
+
+const nodeConfig = {
+  ...commonConfig,
+  target: 'node',
+  output: {
+    libraryTarget: 'commonjs',
+    filename: 'bundle.node.js',
+  },
+};
+
+module.exports = [webConfig, nodeConfig];
