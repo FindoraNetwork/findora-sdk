3dfd9d259180cd032484b08a80129e1d546ec981
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2021-05-07 11:06:30 -0400

    Add remove vscode from gitignore

--- src/config/network.ts
+++ src/config/network.ts
@@ -1,5 +1,12 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-const { PROJECT_ENV } = process.env;
+console.log(`global proces  :) "${global.process}"`);
+const isNodeEnv = typeof process !== 'undefined' && process.release.name === 'node';
+
+let PROJECT_ENV;
+
+if (isNodeEnv) {
+  PROJECT_ENV = process.env;
+}
 export const HOST = PROJECT_ENV || 'dev-staging.dev.findora.org';
 export const SUBMISSION_PORT = '8669';
 export const LEDGER_PORT = '8668';
