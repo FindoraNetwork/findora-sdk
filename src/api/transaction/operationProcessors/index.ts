9713ca3b06981c8465a8f9e26cb6f1bb8910a583
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2021-06-11 17:29:25 -0400

    Implement issue asset processor

--- src/api/transaction/operationProcessors/index.ts
+++ src/api/transaction/operationProcessors/index.ts
@@ -5,8 +5,9 @@ import * as Types from '../types';
 import { processUnsupported, Unsupported } from './unsupported';
 import { processDefineAsset, ProcessedDefineAsset } from './defineAsset';
 import { processTransferAsset, ProcessedTransferAsset } from './transferAsset';
+import { processIssueAsset, ProcessedIssueAsset } from './issueAsset';
 
-export type ProcessedTx = ProcessedDefineAsset | ProcessedTransferAsset | Unsupported;
+export type ProcessedTx = ProcessedDefineAsset | ProcessedTransferAsset | ProcessedIssueAsset | Unsupported;
 
 export type ProcessorType = (op: Types.TxOperation) => Promise<ProcessedTx>;
 
@@ -30,5 +31,6 @@ export const getOperationProcessor = (
 export const processorsMap: TxOperationProcessors = {
   DefineAsset: processDefineAsset,
   TransferAsset: processTransferAsset,
+  IssueAsset: processIssueAsset,
   Unsupported: processUnsupported,
 };
