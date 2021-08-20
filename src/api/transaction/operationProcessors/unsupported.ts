37c5b6207fd95bf1ee46168b67ded0cc96181a39
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2021-08-20 13:02:54 -0400

    feat: fo-68: update dist

--- src/api/transaction/operationProcessors/unsupported.ts
+++ src/api/transaction/operationProcessors/unsupported.ts
@@ -11,8 +11,8 @@ export const processUnsupported = async (op: TxOperation): Promise<Unsupported> 
     result: false,
     type: 'unsupported',
     originalOperation: op,
-    from: 'unknonwn',
-    to: 'unknonwn',
+    from: ['unknonwn'],
+    to: ['unknonwn'],
   };
 
   return data;
