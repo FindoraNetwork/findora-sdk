d7d128bbc04218cde60b61dc6eb4a86409eafc8b
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2021-08-20 12:58:14 -0400

    feat: fo-68: update unsupported to add from and to for compatibility

--- src/api/transaction/operationProcessors/unsupported.ts
+++ src/api/transaction/operationProcessors/unsupported.ts
@@ -11,6 +11,8 @@ export const processUnsupported = async (op: TxOperation): Promise<Unsupported> 
     result: false,
     type: 'unsupported',
     originalOperation: op,
+    from: 'unknonwn',
+    to: 'unknonwn',
   };
 
   return data;
