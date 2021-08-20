3138683596694d07a03ce283fcca9fc57fc1b1a5
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2021-08-20 13:24:06 -0400

    feat: fo-68: update unsupported tx type

--- src/api/transaction/operationProcessors/unsupported.ts
+++ src/api/transaction/operationProcessors/unsupported.ts
@@ -4,6 +4,8 @@ export interface Unsupported {
   result: boolean;
   type: string;
   originalOperation: TxOperation;
+  from: string[];
+  to: string[];
 }
 
 export const processUnsupported = async (op: TxOperation): Promise<Unsupported> => {
