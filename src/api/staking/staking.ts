2ab72590f743af0246c1e1cb61a154de6ccdffc2
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2021-09-02 13:45:18 -0400

    feat: add documentation for staking module

--- src/api/staking/staking.ts
+++ src/api/staking/staking.ts
@@ -4,6 +4,32 @@ import { TransactionBuilder } from '../../services/ledger/types';
 import { WalletKeypar } from '../keypair';
 import * as AssetApi from '../sdkAsset';
 
+/**
+ * Undelegate some staked amount
+ *
+ * @remarks
+ * Using this function, user can initiate (create a transaction builder) an undelegation process
+ *
+ * @example
+ *
+ * ```ts
+ *  const walletInfo = await Keypair.restoreFromPrivateKey(pkey, password);
+ *
+ *  // Define whether or not user desires to withraw akk the funds
+ *  const isFullUnstake = false;
+ *
+ *  const transactionBuilder = await StakingApi.unDelegate(
+ *    walletInfo,
+ *    amount,
+ *    validator,
+ *    isFullUnstake,
+ *  );
+ *
+ *  const resultHandle = await Transaction.submitTransaction(transactionBuilder);
+ * ```
+ *
+ * @returns TransactionBuilder which should be used in `Transaction.submitTransaction`
+ */
 export const unDelegate = async (
   walletInfo: WalletKeypar,
   amount: string,
@@ -61,6 +87,47 @@ export const unDelegate = async (
   return transactionBuilder;
 };
 
+/**
+ * Delegates some funds
+ *
+ * @remarks
+ * Using this function, user can delegate some funds to a given validator.
+ *
+ * In fact, this functionality is almost the same, as `Transaction.sendToAddress` except the fact,
+ * that we are adding one more operation to the transaction builder - `add_operation_delegate`
+ *
+ * @example
+ *
+ * ```ts
+ *  const ledger = await getLedger();
+ *
+ *  // This address is the one, where funds are going to be sent to.
+ *  // Actual `transfer to validator` process would be handled via added `add_operation_delegate` operation
+ *  const delegationTargetAddress = ledger.get_delegation_target_address
+ *
+ *  const walletInfo = await Keypair.restoreFromPrivateKey(pkey, password);
+ *
+ *  const assetCode = await Asset.getFraAssetCode();
+ *
+ *  const assetBlindRules: Asset.AssetBlindRules = {
+ *    isTypeBlind: false,
+ *    isAmountBlind: false
+ *  };
+ *
+ *  const transactionBuilder = await StakingApi.delegate(
+ *    walletInfo,
+ *    delegationTargetAddress,
+ *    amount,
+ *    assetCode,
+ *    validatorAddress,
+ *    assetBlindRules,
+ *  );
+ *
+ *  const resultHandle = await Transaction.submitTransaction(transactionBuilder);
+ * ```
+ *
+ * @returns TransactionBuilder which should be used in `Transaction.submitTransaction`
+ */
 export const delegate = async (
   walletInfo: WalletKeypar,
   address: string,
@@ -82,6 +149,27 @@ export const delegate = async (
   return transactionBuilder;
 };
 
+/**
+ * Claim delegation rewards
+ *
+ * @remarks
+ * When user wants to retrive the rewards that are earned by the delegated funds, he could use this function
+ *
+ * @example
+ *
+ * ```ts
+ *  const walletInfo = await Keypair.restoreFromPrivateKey(pkey, password);
+
+ *  const transactionBuilder = await StakingApi.claim(
+ *    walletInfo,
+ *    amount,
+ *  );
+ *
+ *  const resultHandle = await Transaction.submitTransaction(transactionBuilder);
+ * ```
+ *
+ * @returns TransactionBuilder which should be used in `Transaction.submitTransaction`
+ */
 export const claim = async (walletInfo: WalletKeypar, amount: string): Promise<TransactionBuilder> => {
   const transferFeeOperationBuilder = await Fee.buildTransferOperationWithFee(walletInfo);
 
