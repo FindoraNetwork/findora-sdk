8d443134491940359e95e000e154519757b81da0
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2021-12-02 17:38:55 -0500

    feat: fo-612: extend test helper with logging functionality

--- src/evm/testHelpers.ts
+++ src/evm/testHelpers.ts
@@ -6,6 +6,15 @@ export interface SimpleObject<T> {
   [key: string]: T;
 }
 
+export interface SendOptions {
+  from: string;
+  gasPrice?: string;
+  gas?: number;
+  value?: number | string;
+  nonce?: number;
+  chainId?: number;
+}
+
 export const getRpcPayload = <T>(msgId: number, method: string, extraParams?: T) => {
   const payload = {
     id: msgId,
@@ -16,8 +25,8 @@ export const getRpcPayload = <T>(msgId: number, method: string, extraParams?: T)
   return payload;
 };
 
-export const getPayloadWithGas = (from: string, givenChainId: number) => ({
-  gas: '1000000',
+export const getPayloadWithGas = (from: string, givenChainId: number): SendOptions => ({
+  gas: 1000000,
   gasPrice: '10000000001',
   from,
   chainId: givenChainId,
@@ -43,6 +52,8 @@ let currentTestName = '';
 const start = Date.now();
 
 let lastOperation = Date.now();
+let logsCount = 0;
+let testLogs: string[] = [];
 
 export const msToTime = (s: number) => {
   let sTime = s;
@@ -54,30 +65,38 @@ export const msToTime = (s: number) => {
   const mins = sTime % 60;
   const hrs = (sTime - mins) / 60;
 
-  const formattedHours = hrs > 0 ? `${hrs}:` : '';
-  const formattedMinutes = mins > 0 ? `${mins}:` : '';
+  const formattedHours = hrs > 0 ? `${hrs}h ` : '';
+  const formattedMinutes = mins > 0 ? `${mins}m ` : '';
   const formattedSeconds = secs > 0 ? `${secs}.${ms}s` : `${ms}ms`;
 
   const result = `${formattedHours}${formattedMinutes}${formattedSeconds}`;
   return result;
 };
 
-const testLogs: string[] = [];
-
 export const timeLog = (label?: string, data?: SuperSimpleObject | string | number | boolean) => {
   const currentTime = Date.now();
-  const extraData = typeof data === 'object' ? JSON.stringify(data, null, 2) : data;
+  const extraData = typeof data === 'object' ? JSON.stringify(data, null, 2) : data === false ? '' : data;
 
   const sinceStart = currentTime - start;
   const sinceLastOperation = currentTime - lastOperation;
 
-  const messageToLog = `${testLogs.length + 1}. ${
-    currentTestName ? `"${currentTestName}"` : ''
-  } Last log ${msToTime(sinceLastOperation)} ago. Total is ${msToTime(sinceStart)}${
-    label ? ` - "${label}" ` : ''
-  }${extraData ? `- ${extraData}` : ''}`;
+  const formattedLabel = label ? `${label} ` : '';
+  const lastLog = data === false ? '' : `took ${msToTime(sinceLastOperation)} `;
+  const totalTime = `-> Total run time ${msToTime(sinceStart)}`;
+  const formattedExtra = extraData ? `with data - ${extraData} ` : '';
+  const formattedTestName = currentTestName ? `Test '${currentTestName}' -> ` : '';
+
+  logsCount += 1;
+
+  const messageToLog = `${logsCount}. ${formattedTestName}${formattedLabel}${formattedExtra}${lastLog}${totalTime}`;
 
   testLogs.push(messageToLog);
+
+  lastOperation = currentTime;
+};
+
+export const timeStart = () => {
+  const currentTime = Date.now();
 
   lastOperation = currentTime;
 };
@@ -87,11 +106,13 @@ export const setCurrentTestName = (testName: string) => {
 };
 
 export const afterEachLog = () => {
-  timeLog(`After one test case`);
+  const msg = `Test '${currentTestName}' is finished`;
+  setCurrentTestName('');
+  timeLog(msg, false);
 };
 
 export const afterAllLog = () => {
-  setCurrentTestName('after all hook');
-  timeLog('After all tests');
+  setCurrentTestName('');
+  timeLog('All tests are finished', false);
   console.log('testLogs', testLogs);
 };
