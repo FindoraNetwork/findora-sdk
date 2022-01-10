34ba11bd5ac709968aaf038f1e7da80e9c0149e7
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2022-01-10 12:46:11 -0500

    feat: fo-540: re-enable triple masking wasm in the sdk, and minor changes into get_anon_keypair api

--- dist/api/tripleMasking/tripleMasking.d.ts
+++ dist/api/tripleMasking/tripleMasking.d.ts
@@ -1,1 +1,13 @@
-export declare const genAnonKeys: () => Promise<string>;
+import { AnonKeys } from '../../services/ledger/types';
+interface FormattedAnonKeys {
+    axfrPublicKey: string;
+    axfrSecretKey: string;
+    decKey: string;
+    encKey: string;
+}
+interface KeysResponse {
+    keysInstance: AnonKeys;
+    formatted: FormattedAnonKeys;
+}
+export declare const genAnonKeys: () => Promise<KeysResponse>;
+export {};
