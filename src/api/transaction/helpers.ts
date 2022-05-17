9339e04f897018dd75a1aceac78c86595c4fbf87
Ryan Sun <ryan@findora.org>
2022-05-17 13:19:05 -0700

    documentation: API/Transaction/helper

--- src/api/transaction/helpers.ts
+++ src/api/transaction/helpers.ts
@@ -1,15 +1,69 @@
 import _get from 'lodash/get';
-
 import * as Network from '../network';
 import { TxInfo, TxListDataResult } from '../network/types';
 import * as Types from './types';
 
+/**
+ * Get Transaction List by given transaction data response
+ *
+ * @remarks
+ * Using this function, user can Get Transaction List by given transaction data response
+ *
+ * @example
+ *
+ * ```ts
+ *  const address = 'fra12dsfew';
+ *  const type = 'to';
+ *  const dataResult = await Network.getTxList(address, type);
+ *  // Get tx list
+ *  const txList  = await helpers.getTxListFromResponse();
+ * ```
+ * @param result - transaction data response
+ *
+ * @returns transaction list
+ */
 export const getTxListFromResponse = (result: TxListDataResult): null | TxInfo[] =>
   _get(result, 'response.result.txs', null);
 
+/**
+ * Get Operation List by given parsed transaction
+ *
+ * @remarks
+ * Using this function, user can Get Transaction List by given parsed transaction
+ *
+ * @example
+ *
+ * ```ts
+ *  const address = 'fra12dsfew';
+ *  const type = 'to';
+ *  const dataResult = await Network.getTxList(address, type);
+ *  // Get tx list
+ *  const txList  = await helpers.getTxListFromResponse();
+ *  // Get one parsed tx
+ *  parsedTx = JSON.parse(Base64.decode(txList[0].tx));
+ *  // Get operation Lsit
+ *  opList = helpers.getTxOperationsList(parsedTx);
+ * ```
+ * @param parsedTx - parsed tx info
+ * @returns transaction list
+ */
 export const getTxOperationsList = (parsedTx: Types.ParsedTx): Types.TxOperation[] =>
   _get(parsedTx, 'body.operations', []);
 
+/**
+ * Get Block Time by given block height
+ *
+ * @remarks
+ * Using this function, user can get block time by given block height
+ *
+ * @example
+ *
+ * ```ts
+ *  const blockTime = await helpers.getBlockTime();
+ * ```
+ *
+ * @returns block time
+ */
 export const getBlockTime = async (height: number) => {
   const blockDetailsResult = await Network.getBlock(height);
 
