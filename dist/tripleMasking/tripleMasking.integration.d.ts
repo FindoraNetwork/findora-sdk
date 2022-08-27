7f53f09eb6a4d3db19a75b6a2b9065f996e1e814
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2022-08-27 18:18:37 -0400

    feat: fo-1202: update dist after the merge

--- dist/tripleMasking/tripleMasking.integration.d.ts
+++ dist/tripleMasking/tripleMasking.integration.d.ts
@@ -5,30 +5,11 @@ export declare const getDerivedAssetCode: (asset1Code: string) => Promise<string
 export declare const getAnonKeys: () => Promise<FindoraWallet.FormattedAnonKeys>;
 export declare const createNewKeypair: () => Promise<Keypair.WalletKeypar>;
 export declare const defineCustomAsset: (senderOne: string, assetCode: string) => Promise<void>;
-export declare const issueCustomAsset: (
-  senderOne: string,
-  assetCode: string,
-  derivedAssetCode: string,
-  amount: string,
-) => Promise<void>;
-export declare const validateSpent: (
-  AnonKeys: FindoraWallet.FormattedAnonKeys,
-  givenCommitments: string[],
-) => Promise<boolean>;
+export declare const issueCustomAsset: (senderOne: string, assetCode: string, derivedAssetCode: string, amount: string) => Promise<void>;
+export declare const validateSpent: (AnonKeys: FindoraWallet.FormattedAnonKeys, givenCommitments: string[]) => Promise<boolean>;
 export declare const getSidsForSingleAsset: (senderOne: string, assetCode: string) => Promise<number[]>;
-export declare const createTestBars: (
-  givenSenderOne?: string | undefined,
-  amount?: string,
-  iterations?: number,
-) => Promise<true>;
-export declare const barToAbar: (
-  givenSenderOne?: string | undefined,
-  AnonKeys?: FindoraWallet.FormattedAnonKeys | undefined,
-  givenSids?: number[] | undefined,
-  givenBalanceChange?: string | undefined,
-  givenAssetCode?: string | undefined,
-  isBalanceCheck?: boolean,
-) => Promise<string[]>;
+export declare const createTestBars: (givenSenderOne?: string | undefined, amount?: string, iterations?: number) => Promise<true>;
+export declare const barToAbar: (givenSenderOne?: string | undefined, AnonKeys?: FindoraWallet.FormattedAnonKeys | undefined, givenSids?: number[] | undefined, givenBalanceChange?: string | undefined, givenAssetCode?: string | undefined, isBalanceCheck?: boolean) => Promise<string[]>;
 export declare const abarToAbar: () => Promise<boolean>;
 export declare const abarToAbarMulti: () => Promise<boolean>;
 export declare const abarToAbarFraMultipleFraAtxoForFeeSendAmount: () => Promise<boolean>;
