24316f9ffd42ca13254e32007671fe7f3a98f31f
Giambi <nash9328@hotmail.com>
2022-10-03 15:58:10 +0800

    feat: export rangeHelper

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export * as UtxoHelperService from './services/utxoHelper';
 export * as UtilsService from './services/utils';
 export * as Api from './api';
 export * as Sdk from './Sdk';
+export * as RangeHelper from './services/rangeHelper';
