5f15c01d392ae33051d4c2f8d7ba1738b69c04c4
piedpipernet <akhil@findora.org>
2022-11-06 15:35:30 +0530

    double the execution timeout

--- src/tripleMasking/tripleMasking.integration.spec.ts
+++ src/tripleMasking/tripleMasking.integration.spec.ts
@@ -2,7 +2,7 @@ import * as Integration from './tripleMasking.integration';
 
 import { waitForBlockChange } from '../evm/testHelpers';
 
-const extendedExecutionTimeout = 180000;
+const extendedExecutionTimeout = 360000;
 
 afterAll(async (done: any) => {
   console.log('after all - just waiting for 3 blocks to have all pending requests finished (if any)');
