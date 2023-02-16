82e489ea73ed7a1fdb1ad699ff353d36dd20abad
muniz <1160007652@qq.com>
2023-02-16 10:55:13 +0800

    build: rm dist

--- dist/api/transaction/Builder.d.ts
+++
@@ -1,4 +0,0 @@
-import { AnonTransferOperationBuilder, TransactionBuilder } from '../../services/ledger/types';
-export declare const getBlockHeight: () => Promise<bigint>;
-export declare const getTransactionBuilder: () => Promise<TransactionBuilder>;
-export declare const getAnonTransferOperationBuilder: () => Promise<AnonTransferOperationBuilder>;
